51c549c9794a511f4428e1acaeabe5dbb81e5814
laryssa <laryss.moraes@gmail.com.br>
2023-06-07 18:55:04 -0300

    altera autenticação e aplica estilo na tela login

--- projetoFront/src/pages/loginForm.jsx
+++ projetoFront/src/pages/loginForm.jsx
@@ -1,53 +1,62 @@
-import { useRef, useState } from 'react'
+import { useContext, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import UserContext from '../contexts/UserContext'
 
 export default function LoginForm(props) {
-    const refEmail = useRef()
-    const refSenha = useRef()
-    const [erroEmail, setErroEmail] = useState()
-    const [erroSenha, setErroSenha] = useState()
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const navigate = useNavigate()
+  const { handleLogin } = useContext(UserContext)
+  const [errorLogin, setErrorLogin] = useState("")
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        let valido = true;
-        
-        if (!refEmail.current.value) {
-            setErroEmail("Email obrigatorio")
-            refEmail.current.focus()
-            valido = valido && false;
-        } else {
-            setErroEmail("")
-        }
+  const validaEmail = {
+    required: {
+      value: true,
+      message: 'Email é obrigatorio',
+    },
+    pattern: {
+      value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, //expressão regular de validação de email
+      message: 'Email inválido',
+    }
+  }
 
-        if (!refSenha.current.value) {
-            setErroSenha("Senha obrigatoria")
-            valido = valido && false;
-        } else if (refSenha.current.value.length < 3) {
-            setErroSenha("Senha com no minimo 3 caracteres")
-            valido = valido && false;
-        } else {
-            setErroSenha("")
-        }
+  const validaSenha = {
+    required: {
+      value: true,
+      message: 'Senha é obrigatoria',
+    },
+    minLength: {
+      value: 8,
+      message: 'Senha deve ter no mínimo 8 caracteres',
+    }
+  }
 
-        if (!valido) {
-            return
-        }
-
-        props.onSubmit(event)
+  async function onSubmit(data) { //chama o handle login, tbm deve ser assincrono
+    const { email, senha } = data;
+    setErrorLogin("") //estado do formulario para retorno de erro
+    try {
+      await handleLogin(email, senha)
+      navigate("/home") //url de direcionamento após login com sucesso
+    }catch (error) {
+      setErrorLogin(error.message)
     }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" ref={refEmail}/>
-                {erroEmail && <p>{erroEmail}</p>}
-            </div>
-            <div>
-                <label htmlFor="senha">Senha</label>
-                <input type="password" id="senha" name="senha" ref={refSenha}/>
-                {erroSenha && <p>{erroSenha}</p>}
-            </div>
-            <button>Entrar</button>
-        </form>
-    )
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {errorLogin && <p className="erro">{errorLogin}</p>}
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" {...register("email", validaEmail)} />
+        {errors.email && <p className="erro">{errors.email.message}</p>}
+      </div>
+      <div>
+        <label htmlFor="senha">Senha</label>
+        <input type="password" id="senha" {...register("senha", validaSenha)} />
+        {errors.senha && <p className="erro">{errors.senha.message}</p>}
+      </div>
+      <button>Entrar</button>
+    </form>
+  )
 }
