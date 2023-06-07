19953039219b7e00de53bff681004b6f180b89d3
brunnovaz1 <brunnovaz@gmail.com>
2023-06-06 21:00:15 -0300

    resolucao de conflitos

--- projetoFront/src/components/navbar.jsx
+++ projetoFront/src/components/navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     
       <ul className='listaMenu'>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/home">Home</NavLink>
         </li>
         <li>
           <NavLink to="/notas">Notas</NavLink>
--- projetoFront/src/main.jsx
+++ projetoFront/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {ContatosContextProvider} from './contexts/ContatosContext.jsx'
+import {PedidosContextProvider} from './contexts/PedidosContext.jsx'
 import { UserContextProvider } from './contexts/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
