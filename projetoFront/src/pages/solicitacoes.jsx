238f206f0c2290d6ea26505e7552624ca3b09ffb
brunnovaz1 <brunnovaz@gmail.com>
2023-06-12 17:14:34 -0300

    solicitações criadas são salvas em uma tabela.

--- projetoFront/src/pages/solicitacoes.jsx
+++ projetoFront/src/pages/solicitacoes.jsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react';
 import './css/solicitacoes.css';
 import { PedidosContext } from '../contexts/PedidosContext.jsx';
+import './css/tabelas.css'
+import horario from '../assets/icones/horario.svg'
+
 
 export default function solicitacoes() {
   const { meusPedidos } = useContext(PedidosContext);
@@ -8,16 +11,33 @@ export default function solicitacoes() {
   return (
     <>
       <h2>Historico de Solicitações</h2>
-      <ul>
-        {meusPedidos.map((pedido, index) => (
-          <li className="lista" id={index}>
-            {pedido.nome} - {pedido.msg}
-          </li>
-        ))}
-      </ul>
-    </>
-    
-  );
+        <table>
+          <tbody>
+            <tr>
+              <th>Departamento</th>
+              <th>Solicitação</th>
+              <th>Status</th>
+            </tr>
+            <tr>
+              <th>Financeiro</th>
+              <th>Meu desconto de matrícula não apareceu no boleto.</th>
+              <th>V (icone deferido)</th>
+            </tr>
+            <tr>
+              <th>Secretaria Acadêmica</th>
+              <th>Gostaria de ir para a turma 3ADS2</th>
+              <th>X (icone indeferido)</th>
+            </tr>
+              {meusPedidos.map((pedido, index) =>
+               (<tr key={index}>
+                  <th>{pedido.nome}</th> 
+                  <th>{pedido.msg}</th>
+                  <th novaSolicitacao><img src={horario} alt="status" /></th>
+                </tr>))}
+          </tbody>
+        </table>
+    </>  
+  )
 }
 
-/* https://aluno-online-web.profjosereginal.repl.co/pages/requerimentos.html */
+ /* https://aluno-online-web.profjosereginal.repl.co/pages/requerimentos.html */
--- projetoFront/src/pages/solicitar.jsx
+++ projetoFront/src/pages/solicitar.jsx
@@ -20,11 +20,14 @@ export default function Novo() {
   return (
     <>
       <h2>Nova Solicitação</h2>
-      <form onSubmit={handleSubmit(onSubmit) }>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label for="opcoes">Departamento</label>
-        <select defaultValue={""} name="opcoes" {...register('nome', { required: 'Campo Obrigatório!' })}
+        <select
+          defaultValue={''}
+          name="opcoes"
+          {...register('nome', { required: 'Campo Obrigatório!' })}
         >
-          <option disabled ></option>
+          <option disabled></option>
           <option>Coordenação Curso</option>
           <option>Financeiro</option>
           <option>Secretaria Acadêmica</option>
@@ -36,19 +39,22 @@ export default function Novo() {
           rows="4"
           cols="50"
           placeholder="Digite aqui..."
-          {...register('msg', { required: 'Campo Obrigatório' })}></textarea>
+          {...register('msg', { required: 'Campo Obrigatório' })}
+        ></textarea>
         {errors.msg && <p>{errors.msg.message}</p>}
 
         <button type="submit">Enviar</button>
       </form>
+    </>
+  );
+}
 
-      <h2>Minhas Solicitações</h2>
+{
+  /* <h2>Minhas Solicitações</h2>
       <ul>
         {meusPedidos.map((pedido, index) => (
           <li className="lista" id={index}>{pedido.nome} - {pedido.msg}
           <p>Resposta:</p></li>
         ))}
-      </ul>
-    </>
-  );
+      </ul> */
 }
