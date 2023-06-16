ad3146d839169cd362f9fbc66b69aa9036fe3e42
brunnovaz1 <brunnovaz@gmail.com>
2023-06-15 21:45:55 -0300

    inserção do calendario na pagina home

--- projetoFront/src/pages/home.jsx
+++ projetoFront/src/pages/home.jsx
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import { format, parseISO } from 'date-fns';
+import './css/calendario.css';
+
 export default function home(){
-    return(
-  <section className="ContainerSection">
-    <h1>Pagina Inicial</h1>
-    <p>What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-  </section>
+  
+  const [data, setData]=useState (new Date ());
+  const formattedDate = format(data, 'dd/MM/yyyy');
+
+  return (
+
+    <section className="ContainerSection">
+      <h1>Pagina Inicial</h1>
+      {formattedDate}
+      <Calendar className="calendar"/>
+      <p>What is Lorem Ipsum?
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </section>
   )
 }
