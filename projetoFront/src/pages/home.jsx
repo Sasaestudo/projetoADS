50e79e7044d39491a5d5fe130dd9b1f97b48c12b
brunnovaz1 <brunnovaz@gmail.com>
2023-06-19 14:54:26 -0300

    atualização calendáio

--- projetoFront/src/pages/home.jsx
+++ projetoFront/src/pages/home.jsx
@@ -1,32 +1,79 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import { format, parseISO } from 'date-fns';
-import './css/home.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+import TimePicker from 'react-time-picker';
 
 
+const localizer = momentLocalizer(moment);
 
-export default function home(){
-  
-  const [data, setData]=useState (new Date ());
-  const formattedDate = format(data, 'dd/MM/yyyy');
+const Home = () => {
+  const [events, setEvents] = useState([]);
+
+  const [selectedSlot, setSelectedSlot] = useState(null);
+  const [eventTitle, setEventTitle] = useState('');
+  const [eventTime, setEventTime] = useState(null);
+
+  const handleSelectSlot = (slotInfo) => {
+    setSelectedSlot(slotInfo);
+  };
+
+  const handleEventTitleChange = (e) => {
+    setEventTitle(e.target.value);
+  };
+
+  const handleEventTimeChange = (time) => {
+    setEventTime(time);
+  };
+
+  const handleAddEvent = () => {
+    if (selectedSlot && eventTitle && eventTime) {
+      const newEvent = {
+        start: selectedSlot.start,
+        end: selectedSlot.end,
+        title: eventTitle,
+        time: eventTime,
+      };
+      setEvents([...events, newEvent]);
+      setSelectedSlot(null);
+      setEventTitle('');
+      setEventTime(null);
+    }
+  };
+
+  const eventComponents = events.map((event, index) => (
+    <div key={index}>
+      <span>{event.title}</span>
+      <span>{event.time}</span>
+    </div>
+  ));
 
   return (
-
-    <section className="ContainerSection ">
-       <div className='ContainerHome'>
-
-       </div>
-       <h1>Pagina Inicial</h1>
-     <div className='bloco1'>
-     <article className='calendario'>
+    <div>
+      <div style={{ height: '500px' }}>
+        <Calendar localizer={localizer} events={events} selectable onSelectSlot={handleSelectSlot} />
+      </div>
+      {selectedSlot && (
         <div>
-       <h2>Calendário Acadêmico</h2>
-        {formattedDate}
-        <Calendar className="calendar" />
-        </div> 
-      </article>
-     </div>
-    </section>
-    
-  )
-}
+          <input className='entrada'
+                 type="text"
+                 value={eventTitle}
+                 onChange={handleEventTitleChange}
+                 placeholder="Título do evento"
+          />
+          <TimePicker
+                 value={eventTime}
+                 onChange={handleEventTimeChange}
+                 clearIcon={null}
+                 disableClock={true}
+                 
+          />
+          <button onClick={handleAddEvent}>Adicionar evento</button>
+        </div>
+      )}
+      <div>{eventComponents}</div>
+    </div>
+  );
+};
+
+export default Home;
